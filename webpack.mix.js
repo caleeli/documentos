da868c90ea7d0893cd85738653808b780be5b60b
David Callizaya <davidcallizaya@gmail.com>
2018-08-30 16:20:06 -0400

    Add versioning to the resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .sourceMaps()
-   //.version()
+   .version()
 
 /*mix.webpackConfig({
     devtool: 'inline-source-map'
