9204f654dc7397b692e893fb0ab60eec539a3ee6
David Callizaya <davidcallizaya@gmail.com>
2018-12-04 06:55:42 -0400

    + Dashboard

---
+++ resources/assets/js/modules/Dashboard.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="content">
+        <div class="row">
+            <div class="col-lg-2 col-md-4 col-sm-6 col-xs-12 text-center" v-for="link in accessLinks">
+                <router-link :to="link.href">
+                    <img class="access-link" :src="link.icon"><br>
+                    {{link.text}}
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        path: "/",
+        data() {
+            return {
+                accessLinks: [
+                    {
+                        icon: require("../../images/hoja-de-ruta.svg"),
+                        text: "HOJA EXTERNA",
+                        href: "/HojaRuta/externa",
+                    },
+                    {
+                        icon: require("../../images/busqueda.svg"),
+                        text: "BUSQUEDA EXTERNA",
+                        href: "/HojaRutaBusqueda/externa",
+                    },
+                    {
+                        icon: require("../../images/reporte.svg"),
+                        text: "REPORTE EXTERNA",
+                        href: "/HojaRutaReporte/externa",
+                    },
+                    {
+                        icon: require("../../images/hoja-de-ruta.svg"),
+                        text: "HOJA INTERNA",
+                        href: "/HojaRuta/interna",
+                    },
+                    {
+                        icon: require("../../images/busqueda.svg"),
+                        text: "BUSQUEDA INTERNA",
+                        href: "/HojaRutaBusqueda/interna",
+                    },
+                    {
+                        icon: require("../../images/reporte.svg"),
+                        text: "REPORTE INTERNA ",
+                        href: "/HojaRutaReporte/interna",
+                    },
+                ]
+            };
+        }
+    };
+</script>
+
+<style lang="scss">
+    .access-link {
+        margin-top: 1em;
+        width: 64px;
+    }
+</style>
