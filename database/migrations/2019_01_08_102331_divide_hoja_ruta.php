597b3022737511aa2b8b631b7172664f2691de74
David Callizaya <davidcallizaya@gmail.com>
2019-01-08 08:40:19 -0400

    Fix migration

--- database/migrations/2019_01_08_102331_divide_hoja_ruta.php
+++ database/migrations/2019_01_08_102331_divide_hoja_ruta.php
@@ -13,8 +13,7 @@ class DivideHojaRuta extends Migration
     public function up()
     {
         DB::connection('hr')->transaction(function() {
-            $this->fixDate('hoja_ruta', 'fecha');
-            $this->fixDate('hoja_ruta', 'conclusion');
+            $this->fixDate('hoja_ruta', ['fecha', 'conclusion']);
             DB::connection('hr')->statement('CREATE TABLE hoja_ruta_interna LIKE hoja_ruta;');
             DB::connection('hr')->statement('INSERT hoja_ruta_interna SELECT * FROM hoja_ruta WHERE tipo="interna";');
             DB::connection('hr')->statement('ALTER TABLE `hoja_ruta` RENAME TO `hoja_ruta_externa`;');
@@ -35,10 +34,16 @@ class DivideHojaRuta extends Migration
     /**
      * Fix date nullable
      */
-    private function fixDate($table, $column)
+    private function fixDate($table, $columns)
     {
-        DB::connection('hr')->statement('UPDATE `' . $table . '` SET `' . $column . '` = "1797-07-07" WHERE `' . $column . '`="0000-00-00";');
-        DB::connection('hr')->statement('ALTER TABLE `' . $table . '` CHANGE `' . $column . '` `' . $column . '` date NULL;');
-        DB::connection('hr')->statement('UPDATE `' . $table . '` SET `' . $column . '` = null WHERE `' . $column . '`="1797-07-07";');
+        foreach ($columns as $column) {
+            DB::connection('hr')->statement('UPDATE `' . $table . '` SET `' . $column . '` = "1797-07-07" WHERE `' . $column . '`="0000-00-00";');
+        }
+        foreach ($columns as $column) {
+            DB::connection('hr')->statement('ALTER TABLE `' . $table . '` CHANGE `' . $column . '` `' . $column . '` date NULL;');
+        }
+        foreach ($columns as $column) {
+            DB::connection('hr')->statement('UPDATE `' . $table . '` SET `' . $column . '` = null WHERE `' . $column . '`="1797-07-07";');
+        }
     }
 }
