09553cf8b475e314abcbb16cac743f779d7e01cb
David Callizaya <davidcallizaya@gmail.com>
2019-01-08 08:46:31 -0400

    Clear localstorage on login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,52 +62,55 @@
   </form>
 </panel -->
 <body class="login-page">
-  <div class="login-container">
-    <div class="login-content animation bounceInDown">
-      <div class="login-header m-b-30">
-        <a class="logo" href="components.html" title="SUBCEP">
-          <img src="/images/logo128.png" alt="SUBCEP" title="SUBCEP" style="height:40px">
-        </a>
-      </div>
-
-      <div class="login-form">
-        <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Ingreso') }}">
-          @csrf
-          <fieldset>
-            <div class="form-group">
-              <input name="username" id="username" value="" class="form-control input-lg" placeholder="Usuario">
-              @if ($errors->has('email'))
-              <span class="invalid-feedback" role="alert">
-                <strong>{{ $errors->first('email') }}</strong>
-              </span>
-              @endif
-            </div>
-            <div class="form-group">
-              <input name="password" id="password" class="form-control input-lg" placeholder="Contraseña" type="password">
-              @if ($errors->has('password'))
-              <span class="invalid-feedback" role="alert">
-                <strong>{{ $errors->first('password') }}</strong>
-              </span>
-              @endif
-            </div>
-            <div class="form-group clearfix">
-              <div class="checkbox checkbox-primary checkbox-replace pull-left">
-                <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                <label class="form-check-label" for="remember">Recuérdame</label>
-              </div>
-
-              <div class="m-t-10 pull-right">
-                <a class="text-primary" href="{{ route('password.request') }}" title="Recuperar contraseña">{{ __('¿Olvidaste tu contraseña?') }}</a>
-              </div>
+    <div class="login-container">
+        <div class="login-content animation bounceInDown">
+            <div class="login-header m-b-30">
+                <a class="logo" href="components.html" title="SUBCEP">
+                    <img src="/images/logo128.png" alt="SUBCEP" title="SUBCEP" style="height:40px">
+                </a>
             </div>
 
-            <button type="submit" class="btn btn-primary btn-rounded">
-              {{ __('Ingresar') }}
-            </button>
-          </fieldset>
-        </form>
-      </div>
+            <div class="login-form">
+                <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Ingreso') }}">
+                    @csrf
+                    <fieldset>
+                        <div class="form-group">
+                            <input name="username" id="username" value="" class="form-control input-lg" placeholder="Usuario">
+                            @if ($errors->has('email'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <input name="password" id="password" class="form-control input-lg" placeholder="Contraseña" type="password">
+                            @if ($errors->has('password'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                        <div class="form-group clearfix">
+                            <div class="checkbox checkbox-primary checkbox-replace pull-left">
+                                <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                       <label class="form-check-label" for="remember">Recuérdame</label>
+                            </div>
+
+                            <div class="m-t-10 pull-right">
+                                <a class="text-primary" href="{{ route('password.request') }}" title="Recuperar contraseña">{{ __('¿Olvidaste tu contraseña?') }}</a>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary btn-rounded">
+                            {{ __('Ingresar') }}
+                        </button>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
     </div>
-  </div>
+    <script>
+        window.localStorage.clear();
+    </script>
 </body>
 @endsection
