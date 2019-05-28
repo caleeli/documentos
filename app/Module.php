5403a474e70f9f4626f97896ef61c0c0d4c9f30f
David Callizaya <davidcallizaya@gmail.com>
2019-05-28 15:50:55 -0400

    Add menu module models

---
+++ app/Module.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use App\Traits\AutoTableTrait;
+use App\Traits\SaveUserTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Module extends Model
+{
+    use SaveUserTrait;
+    use SoftDeletes;
+    use AutoTableTrait;
+
+    public $incrementing = true;
+    public $timestamps = true;
+    protected $connection = 'hr';
+    protected $table = 'modules';
+}
