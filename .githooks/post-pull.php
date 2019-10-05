d5720da370f261915c28bb1bd29946159652128e
David Callizaya <davidcallizaya@gmail.com>
2019-10-05 14:24:24 -0400

    Se require dumpautoload antes de migrate seed

--- .githooks/post-pull.php
+++ .githooks/post-pull.php
@@ -3,7 +3,7 @@ run(
     // Update libraries
     onchange(['composer.json'], 'composer install;') .
     // Rebuild database
-    onchange(['database/migrations', 'database/seeds'], 'php artisan migrate:fresh --seed;') .
+    onchange(['database/migrations', 'database/seeds'], 'composer dumpautoload;php artisan migrate:fresh --seed;') .
     // Send email after update
     email('angelitacc27@gmail.com', 'Aplicación actualizada', 'El código de la aplicación fue actualizado en el servidor.')
 );
