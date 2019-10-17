050a9cdc2a3a4966c687af927709f900b38b5e30
David Callizaya <davidcallizaya@gmail.com>
2019-10-17 11:15:35 -0400

    Correccion nombre de secuencia

--- database/migrations/2019_10_17_114535_fix_entidades.php
+++ database/migrations/2019_10_17_114535_fix_entidades.php
@@ -12,7 +12,7 @@ class FixEntidades extends Migration
      */
     public function up()
     {
-        DB::select("SELECT setval('adm_entidad_ent_id_seq1', max(ent_id)) FROM adm_entidad;");
+        DB::select("SELECT setval('adm_entidad_ent_id_seq', max(ent_id)) FROM adm_entidad;");
         Schema::table('adm_entidad', function ($table) {
             $table->integer('user_add')->nullable()->default(null);
             $table->integer('user_mod')->nullable()->default(null);
